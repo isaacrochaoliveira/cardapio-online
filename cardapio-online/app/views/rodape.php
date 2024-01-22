f7830be0ce95347d474228b0d29c321c109faf41
isaacrochaoliveira <120681524+isaacrochaoliveira@users.noreply.github.com>
2024-01-22 00:12:53 -0300

    CP

--- cardapio-online/app/views/rodape.php
+++ cardapio-online/app/views/rodape.php
@@ -1,11 +1,12 @@
-<footer class="bg-gray">
-	<div class="py-5 px-5">
-		<div>
-			<h2 class="font-satisfy size-40pt">LoveChurch</h2>			
-		</div>
-		<hr>
-		<div>
-			<p>&copy; <?= Date('Y') ?> LoveChurch - <?= VERSION ?></p>
-		</div>
-	</div>
+<footer id="footer">
+    <div class="container">
+        <div>
+            <h3 class="font-satisfy size-60pt">Los Pacos</h3>
+            <h1 class="size-40pt">(62) 99656-7587</h1>
+        </div>
+        <hr>
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas vestibulum ac leo a finibus. Nullam tempus rutrum purus quis faucibus. Vestibulum eget ante cursus, viverra elit et, tempus nibh. Curabitur ornare volutpat risus eget interdum</p>
+        </div>
+    </div>
 </footer>
